e11d6d54aa54665c1703c31c63cac081cdec1c20
Flávio Félix <fau_felix30@hotmail.com>
2023-02-01 14:03:46 -0300

    Fazendo o requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
-};
+function countAnimals(animal) {
+  const { species } = data;
+  const object = {};
+  if (animal === undefined) {
+    species.forEach(({ name, residents }) => {
+      object[name] = residents.length;
+    });
+    return object;
+  }
+  const { residents } = species.find(({ name }) => name === animal.specie);
+  if (animal.sex) {
+    return residents.filter(({ sex }) => sex === animal.sex).length;
+  }
+  return residents.length;
+}
 
 module.exports = countAnimals;
