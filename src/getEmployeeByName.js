fc69cb567dcf99632831d16aa1939bea022d1ba3
Flávio Félix <fau_felix30@hotmail.com>
2023-02-01 13:14:57 -0300

    Feito o requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const { employees } = data;
+  return employees
+    .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
 };
 
 module.exports = getEmployeeByName;
